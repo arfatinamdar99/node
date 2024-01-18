fd70be1f1197891c4c003c3f1ab3a9d115837c86
arfat <mohdarfatinamdar@gmail.com>
2024-01-18 11:55:33 +0530

    sdgf

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var express = require('express');
- 
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
- res.send('hello');
-=======
- res.send('hello world');
->>>>>>> 81cc62a2b3bca35b6e6ca46ccae7552481ee4ccb
+ res.send('hello there');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
- 
 module.exports = app;
